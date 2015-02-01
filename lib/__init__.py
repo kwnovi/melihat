96a5a2502c68c482c551bddb707e786e765675ac
kwnovi <k.wnovi@gmail.com>
2015-02-01 15:14:30 +0100

    structure, get_id

---
+++ lib/__init__.py
@@ -0,0 +1,38 @@
+ # -*- coding: utf-8 -*-
+
+from urlparse import urlparse
+
+def get_id(value):
+	"""
+	Retourne l'ID d'une vidéo YouTube à partir d'un lien 
+
+	Formats valides : 
+
+	- http://youtu.be/5VtsTW_t9xk
+	- http://www.youtube.com/embed/5VtsTW_t9xk"
+	- http://www.youtube.com/v/5VtsTW_t9xk?version=3&amp;hl=en_US"
+	- https://www.youtube.com/watch?v=5VtsTW_t9xk"
+
+	"""
+	query = urlparse(value)
+
+	if query.hostname=="youtu.be":
+		return query.path[1:]
+	if query.hostname in ('www.youtube.com', 'youtube.com'):
+		if query.path == "/watch":
+			return query.query[3:]
+		if (query.path[:7] == "/embed/" or query.path[:3] == "/v/") :
+			return query.path.split("/")[2]
+	return None
+
+# CRÉER UNE CLASSE VIDÉO (NOM, DURÉE, AUTEUR, NOMBRE DE VUE ETC..) 
+# DEF IMPORT_JSON
+# DEF BROWSE_SUGGGESTION
+
+if __name__ == '__main__':
+
+	url, url2, url3, url4 ="http://youtu.be/5VtsTW_t9xk","http://www.youtube.com/embed/5VtsTW_t9xk","http://www.youtube.com/v/5VtsTW_t9xk?version=3&amp;hl=en_US","https://www.youtube.com/watch?v=5VtsTW_t9xk"
+
+	for x in (url, url2, url3, url4):
+		print get_id(x)
+
---
+++ main.py
@@ -0,0 +1,10 @@
+ # -*- coding: utf-8 -*-
+
+import lib as _video
+
+url="http://youtu.be/5VtsTW_t9xk"
+
+print _video.get_id(url)
+
+
+
